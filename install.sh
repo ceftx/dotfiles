2f42b4a4cd4b04a9d3c584ba8857bfe62464807f
Claudio Mora <claudiocmorab@gmail.com>
2025-10-07 19:42:50 -0400

    add polybar to install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ ln -sf ~/dotfiles/config/fish ~/.config/fish
 ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
 ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/config/picom ~/.config/picom
+ln -sf ~/dotfiles/config/qtile ~/.config/qtile
+ln -sf ~/dotfiles/config/polybar ~/.config/polybar
 
 echo "Dotfiles instalados correctamente"
-
