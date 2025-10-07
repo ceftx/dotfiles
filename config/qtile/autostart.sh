c97e4185c1823e57058de745363fb3b94b78c70e
Claudio Mora <claudiocmorab@gmail.com>
2025-10-07 19:40:12 -0400

    add keyboard layout in autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#/bin/sh
 
 # polkit
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# bar
+polybar &
+
+# keyboard layout
+setxkbmap -layout us -variant intl &
 
 # wallpaper with feh
 ~/.fehbg &
